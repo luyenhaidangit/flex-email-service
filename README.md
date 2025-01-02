cc8ef3f64d19d1dabba5ff1c656ba6158b972427
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2025-01-02 13:57:28 +0700

    Khởi tạo dự án spring boot

---
+++ README.md
@@ -0,0 +1,2 @@
+# flex-email-service
+ 
---
+++ src/main/java/com/fss/flexemailservice/FlexEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fss.flexemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlexEmailServiceApplication.class, args);
+    }
+
+}
