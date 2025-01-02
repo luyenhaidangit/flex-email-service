f3fc3fdcf71ec828e8d06689139ac225243b3210
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2025-01-02 17:56:37 +0700

    Triển khai query oracle database với spring

--- src/main/java/com/fss/flexemailservice/controller/HelloController.java
+++ src/main/java/com/fss/flexemailservice/controller/HelloController.java
@@ -3,12 +3,23 @@ package com.fss.flexemailservice.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/check")
+    public void checkConnection() {
+        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM DUAL", Integer.class);
+        System.out.println("Connected to DB. Count from DUAL: " + count);
     }
 }
